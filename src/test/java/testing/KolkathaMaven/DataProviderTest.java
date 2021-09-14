321aba842e2659960fd70861e1f19dacdffd764c
kpr369 <kpr369@rediffmail.com>
2021-09-14 19:45:28 +0530

    Test 2 Changes

--- src/test/java/testing/KolkathaMaven/DataProviderTest.java
+++ src/test/java/testing/KolkathaMaven/DataProviderTest.java
@@ -16,6 +16,8 @@ public class DataProviderTest {
 
 		driver.get("https://www.google.com/");
 		driver.findElement(By.name("q")).sendKeys(query);
+		driver.findElement(By.name("q")).sendKeys("Python");
+
 	}
 	
 		
